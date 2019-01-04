d4a4d3ec4e7bd76312bc6586aa60e235a3739c6b
stoffee <chris@roadkill.org>
2019-01-03 16:36:28 -0800

    fixed main

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ terraform {
 # ---------------------------------------------------------------------------------------------------------------------
 
 module "nomad_and_consul_servers" {
-  source = "git::git@github.com:stoffee/terraform-google-consul.git//modules/consul-cluster?ref=v0.0.1"
+  source = "git::git@github.com:stoffee/terraform-google-consul.git//modules/consul-cluster"
 
   gcp_zone = "${var.gcp_zone}"
 
